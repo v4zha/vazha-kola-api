dcb5f69010a4bc56cafa06380ebcc0eee5405b3f
v4zha <arvin_j4848@protonmail.com>
2022-03-14 09:33:09 +0530

    test

--- src/db_mod/db_handler.rs
+++ src/db_mod/db_handler.rs
@@ -39,7 +39,7 @@ pub async fn login_user(
 }
 
 fn check_pass(resp:&str,pass:&str)->bool{
-    println!("{}",&pass);
+    println!("{} >>\n {}",&pass,resp);
     let resp=resp.as_bytes();
     let val=argon2::verify_encoded(&pass, resp);
     val.unwrap()
